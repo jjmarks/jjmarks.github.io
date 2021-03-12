6fc6a4b5a8c3283120a37b6667ac2c9d9623e60d
jjmarks <jjmarks@pm.me>
2021-03-12 11:37:29 +0000

    change landing page phrasing

--- project/components/LandingMain.vue
+++ project/components/LandingMain.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="rounded-lg bg-gray-900 bg-opacity-50 text-gray-100 text-lg justify-center flex w-full px-10 py-10">
-        My name is Joe, and I'm a maths undergraduate interested in statistics and finance.
+        My name is Joe, and I'm a mathematics undergraduate interested in statistics and finance.
     </div>
 </template>
 
