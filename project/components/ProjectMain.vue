81e802308066639fba913b9247dbe522a0151dcd
jjmarks <jjmarks@pm.me>
2021-03-26 23:03:42 +0000

    add BS project

--- project/components/ProjectMain.vue
+++ project/components/ProjectMain.vue
@@ -2,7 +2,7 @@
   <div class="h-full rounded-lg bg-gray-900 bg-opacity-75 text-gray-100 text-lg justify-center flex w-full px-10 py-10">
     <div class="flex items-center h-full">
       <div class="space-y-2">
-        <p>Current: Black-Scholes Pricing Engine (Fortran)</p>
+        <p>Current: <a class="text-blue-200 font-bold" target="_blank" href="https://github.com/jjmarks/Black-Scholes-F95">Black-Scholes Pricing Engine (Fortran)</a></p>
         <p>Past: -</p>
       </div>
     </div>
