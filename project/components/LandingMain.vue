b37c13c6aa999e3140d6fdaccd2bd7c9ba8ea070
jjmarks <45345888+jjmarks@users.noreply.github.com>
2021-11-16 23:02:19 +0000

    update landing page bio

--- project/components/LandingMain.vue
+++ project/components/LandingMain.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="h-full rounded-lg bg-gray-900 bg-opacity-75 text-gray-100 text-lg justify-center flex w-full px-10 py-10">
     <div class="flex items-center h-full">
-      My name is Joe, and I'm a mathematics undergraduate interested in statistics and finance.
+      My name is Joe, and I'm a mathematics graduate interested in statistics, data science and finance. I am currently working as a Data Scientist for the credit risk modelling team at Atom Bank.
     </div>
   </div>
 </template>
