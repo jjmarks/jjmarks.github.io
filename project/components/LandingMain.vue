08936f48ed88abe36199e9f6ee0e5b9cbbf47e13
jjmarks <45345888+jjmarks@users.noreply.github.com>
2021-11-17 13:25:25 +0000

    add Atom Bank link

--- project/components/LandingMain.vue
+++ project/components/LandingMain.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="h-full rounded-lg bg-gray-900 bg-opacity-75 text-gray-100 text-lg justify-center flex w-full px-10 py-10">
     <div class="flex items-center h-full">
-      My name is Joe, and I'm a mathematics graduate interested in statistics, data science and finance. I am currently working as a Data Scientist for the credit risk modelling team at Atom Bank.
+      <p>My name is Joe, and I'm a mathematics graduate interested in statistics, data science and finance. I am currently working as a Data Scientist for the credit risk modelling team at <a class="text-blue-200" target="_blank" href="https://www.atombank.co.uk/">Atom Bank</a>.</p>
     </div>
   </div>
 </template>
