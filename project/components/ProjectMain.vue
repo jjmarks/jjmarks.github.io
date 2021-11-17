2a85cf9a957a84c3381a2405a05bc77c91f772c2
jjmarks <45345888+jjmarks@users.noreply.github.com>
2021-11-17 13:27:16 +0000

    update project list

--- project/components/ProjectMain.vue
+++ project/components/ProjectMain.vue
@@ -2,8 +2,8 @@
   <div class="h-full rounded-lg bg-gray-900 bg-opacity-75 text-gray-100 text-lg justify-center flex w-full px-10 py-10">
     <div class="flex items-center h-full">
       <div class="space-y-2">
-        <p>Current: <a class="text-blue-200 font-bold" target="_blank" href="https://github.com/jjmarks/Black-Scholes-F95">Black-Scholes Pricing Engine (Fortran)</a></p>
-        <p>Past: -</p>
+        <p>Current: -</p>
+        <p>Past: <a class="text-blue-200 font-bold" target="_blank" href="https://github.com/jjmarks/Black-Scholes-F95">Black-Scholes Pricing Engine (Fortran)</a></p>
       </div>
     </div>
   </div>
